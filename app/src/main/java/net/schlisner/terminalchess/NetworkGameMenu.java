172092d67ccc99a9e055469d7af879568d7b4823
coles <coles@windward.net>
2017-07-28 14:59:55 -0600

    Start work on game creation/joining

---
+++ app/src/main/java/net/schlisner/terminalchess/NetworkGameMenu.java
@@ -0,0 +1,36 @@
+package net.schlisner.terminalchess;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class NetworkGameMenu extends AppCompatActivity {
+    String uuid;
+    SharedPreferences sharedPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_network_game_menu);
+        sharedPref = this.getSharedPreferences("Dank Memes(c)", MODE_PRIVATE);
+        uuid = sharedPref.getString("uuid", "");
+
+
+
+    }
+
+}
